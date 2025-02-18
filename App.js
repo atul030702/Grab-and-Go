4e2fac00094a452d5b9e34fe8e8d016eb687110e
atul030702 <singhatul0307@gmail.com>
2025-02-18 21:30:09 +0530

    second episode

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div", 
     {id: "parent"},[
         React.createElement(
             "div", 
             {id: "child"},[
-            React.createElement("h1", {}, "I'm a H1 Tag! || Atul Singh"),
-            React.createElement("h2", {}, "I'm a h2 tag!|| Brother of h1"),
+            React.createElement("h1", {}, "This is Namaste React🚀 || Atul Singh"),
+            React.createElement("h2", {}, "I'm a h2 tag using react 😃|| Brother of h1"),
         ]),
         React.createElement(
             "div", 
