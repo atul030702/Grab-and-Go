3875b3f8bd42cc65423414d1e266c9869caf389c
atul030702 <singhatul0307@gmail.com>
2025-02-20 21:04:33 +0530

    food app with hardcoded value

--- App.js
+++ App.js
@@ -1,31 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element => Object => render => HTML Element
-// const heading = (<h1 id="heading" className="head">
-//     Hello react using JSX || Namaste React 🚀
-// </h1>);
-// console.log(heading);
+/*
+Header
+    -Logo
+    -Nav Items
+Body
+    -Search 
+    -Restaurant card container
+        -Restaurant card
+            -Img
+            -Name of Res, Star rating, cuisine, delivery time
+Footer
+    -Copyright
+    -Links
+    -Address
+    -Contact
+*/
 
-// //React Component(functional)
-// const HeadingComponent = () => {
-//     return <h1 className="heading">Namaste React functional component</h1>;
-// };
-
-// const HeadingComponent1 = () => (
-//     <div id="container">
-//         <HeadingComponent />
-//         <h1 className="heading">Namaste React from Nested functional component</h1>
-//     </div>
-// );
-
-const HeaderComponent = () => {
-    return (<div className="container">
-        <img src="https://www.airforce-technology.com/wp-content/uploads/sites/6/2017/09/1l-image-24.jpg" alt="su30mki"/>
-        <input type="text" placeholder="Search..."/>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"/></svg>
-    </div>)
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All" alt="logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <img src="https://images.squarespace-cdn.com/content/v1/612d4825ee7c3b7ba3e215b7/33a0e76c-d670-4bd8-b150-64b450896b99/Delicious+food.png?format=2500w" alt="indian-food"/>
+            <h3>Meghana Foods</h3>
+            <h4>Biryani, North Indian, Asian</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="restaurant-container">
+                < RestaurantCard />
+                < RestaurantCard />
+                < RestaurantCard />
+                < RestaurantCard />
+                < RestaurantCard />
+                < RestaurantCard />
+                < RestaurantCard />
+                < RestaurantCard />
+                < RestaurantCard />
+            </div>
+        </div>
+    );
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            {/* Header */}
+            <Header />
+            {/* Body */}
+            <Body />
+            {/* Footer */}
+        </div>
+    )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeaderComponent />);
+root.render(<AppLayout />);
