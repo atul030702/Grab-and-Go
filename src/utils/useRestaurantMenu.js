fcba0e3f65b55ea87befdea2ce1d77e587a4094f
atul030702 <singhatul0307@gmail.com>
2025-04-06 21:03:06 +0530

    removed all the logs and unused code

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -10,7 +10,7 @@ const useRestaurantMenu = (resId) => {
               //const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.6183666&lng=85.0999572&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`);
               const response = await data.json();
               setResInfo(response?.data);
-              console.log(response);
+
             } catch (error) {
               console.error("Error fetching menu:", error);
             }
